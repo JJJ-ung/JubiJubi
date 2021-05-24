2f10c5abeaceb7d603cf86fbf5c5c271cd831d19
Meenki <meenki98@gmail.com>
2021-05-24 19:02:16 +0900

    210524 1902

--- Client/Bitcoin.py
+++ Client/Bitcoin.py
@@ -11,10 +11,24 @@ class CoinInfo():
                 if coin['market'].lower().endswith(str.lower()) or coin['korean_name'] == str or coin['english_name'].lower() == str.lower():
                     return coin
 
-
 class Bitcoin(threading.Thread):
+    intervalTable = ["minute1", "minute3", "minute5", "minute10", "minute15", "minute30", "minute60", "minute240", "day", "week", "month"]
+
     def __init__(self, info):
         self.ticker = info['market']
         self.koreanName = info['korean_name']
         self.englishName = info['english_name']
+        self.interval = "day"
+        self.lstDailyData = list(pyupbit.get_ohlcv(self.ticker, count=5, interval=self.interval)['close']) # 날짜 오름차순
+
+        self.setInterval()
         
+    # 0.minute1, 1.minute3, 2.minute5, 3.minute10, 4.minute15, 5.minute30, 6.minute60, 7.minute240, 8.day, 9.week, 10.month
+    def setInterval(self, cnt=8):
+        self.interval = Bitcoin.intervalTable[cnt]
+        df = pyupbit.get_ohlcv(self.ticker, interval=self.interval)
+        self.chartData = list(df['close'])
+
+result = CoinInfo.SearchCoin("btc")
+if result is not None:
+    result = Bitcoin(result)
