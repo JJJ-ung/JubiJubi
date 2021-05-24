99ac1b6fa8a85ac66aba518da7a7b7e30e1be7f3
Meenki <meenki98@gmail.com>
2021-05-24 23:42:08 +0900

    210524 2342

--- Client/Bitcoin.py
+++ Client/Bitcoin.py
@@ -15,21 +15,31 @@ class Bitcoin(threading.Thread):
     intervalTable = ["minute1", "minute3", "minute5", "minute10", "minute15", "minute30", "minute60", "minute240", "day", "week", "month"]
 
     def __init__(self, info):
+        super().__init__()
         self.ticker = info['market']
         self.koreanName = info['korean_name']
         self.englishName = info['english_name']
         self.interval = "day"
+        self.coinData = pyupbit.WebSocketManager("ticker", [self.ticker])
+        self.coinData.get()
         self.lstDailyData = list(pyupbit.get_ohlcv(self.ticker, count=6, interval=self.interval)['close']) # 날짜 오름차순
         self.lstDailyVolume = list(pyupbit.get_ohlcv(self.ticker, count=6, interval=self.interval)['volume'])
-
-        self.setInterval()
         
     # 0.minute1, 1.minute3, 2.minute5, 3.minute10, 4.minute15, 5.minute30, 6.minute60, 7.minute240, 8.day, 9.week, 10.month
     def setInterval(self, cnt=8):
         self.interval = Bitcoin.intervalTable[cnt]
-        df = pyupbit.get_ohlcv(self.ticker, interval=self.interval)
-        self.chartData = list(df['close'])
 
-result = CoinInfo.SearchCoin("btc") 
-if result is not None:
-    result = Bitcoin(result)
+    def getPrice(self):
+        return self.coinData.get('trade_price')
+
+    def run(self):
+        while True:
+            self.chartData = list(pyupbit.get_ohlcv(self.ticker, interval=self.interval)['close'])
+
+
+if __name__ == '__main__':
+    result = CoinInfo.SearchCoin("btc") 
+    if result is not None:
+        result = Bitcoin(result)
+        result.daemon = True
+        result.start()
