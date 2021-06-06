7df618e96db5d798566acc44ff633d90c351b582
JJJung <84189619+JJJ-ung@users.noreply.github.com>
2021-06-06 19:37:45 +0900

    210606_1937
    러먀ㅐ허ㅑ넝ㅎㅁㄴㅇㅎ

--- Client/AppGUI/StockPage.py
+++ Client/AppGUI/StockPage.py
@@ -352,6 +352,7 @@ class Page:
         self.ax.ticklabel_format(axis='y', style='plain')
         if self.anim is not None:
             self.anim._stop()
+        self.anim = None
         self.Canvas.draw()
 
     def SetAniGraph(self):
